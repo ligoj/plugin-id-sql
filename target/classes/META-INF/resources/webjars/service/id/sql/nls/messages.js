e45374ed417ac1a44e95065e1c93d14944494474
Fabrice Daugan <fabrice.daugan@gmail.com>
2023-08-18 11:56:40 +0200

    Cleanup

---
+++ target/classes/META-INF/resources/webjars/service/id/sql/nls/messages.js
@@ -0,0 +1,11 @@
+/*
+ * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
+ */
+define({
+	root: {
+		'service:id:sql:salt-length': 'Salt string length used to build the credential hash',
+		'service:id:sql:hash-iteration': 'Hash iteration count',
+		'service:id:sql:key-length': 'Hash key length',
+	},
+	fr: true
+});
